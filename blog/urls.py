bc130fee022a411bc46d2abbc0a9b0854fc9af5a
Vasanth <vasanthk132@gmail.com>
2021-08-24 05:57:11 +0530

    update path for individual blogs

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog import views
 app_name="blog"
 urlpatterns = [
     path('', views.all_blogs_list, name="all_blogs_list"),
-    #path('<int:pk>', views.detail_view, name="detail_view"),
+    path('<int:pk>', views.detail_view, name="detail_view"),
 ]
